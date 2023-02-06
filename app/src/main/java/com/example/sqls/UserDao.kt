40b5c669f0f3d5c8379a377ea1fc442d64d5a393
张小龙 <2247711933@qq.com>
2023-02-06 17:24:20 +0800

    room数据库添加

--- app/src/main/java/com/example/sqls/UserDao.kt
+++ app/src/main/java/com/example/sqls/UserDao.kt
@@ -10,6 +10,7 @@ interface UserDao {
     @Query("SELECT * FROM user WHERE id IN (:userIds)")
     suspend fun loadAllByIds(userIds: IntArray): List<User>
 
+
     @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
             "last_name LIKE :last LIMIT 1")
     suspend fun findByName(first: String, last: String): User
